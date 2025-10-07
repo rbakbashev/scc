40bcdab821274a80d6364beed1ec2bf67fe1002c
Ruslan Akbashev <ruslashev@gmail.com>
2025-10-07 23:59:29 +0400

    output: merge addresses and bytes output into one struct

--- src/output.rs
+++ src/output.rs
@@ -9,10 +9,11 @@ use crate::elf::construct_elf;
 use crate::ir::{ArithOp, format_arith};
 use crate::utils::{CheckError, error};
 
-struct Addresses
+struct Output
 {
-	map: HashMap<String, usize>,
+	addresses: HashMap<String, usize>,
 	relocations: Vec<Relocation>,
+	bytes: Vec<u8>,
 }
 
 struct Relocation
@@ -29,14 +30,41 @@ pub struct Code
 	pub entrypoint: usize,
 }
 
-fn addresses_get_label(addresses: &Addresses, label: u16) -> Option<usize>
+fn empty_output() -> Output
 {
-	addresses.map.get(&format!("L{label}")).copied()
+	Output { addresses: HashMap::new(), relocations: Vec::new(), bytes: Vec::new() }
 }
 
-fn addresses_push_reloc(addr: &mut Addresses, label: u16, out: &[u8], offset: usize, instr_len: i32)
+fn push(out: &mut Output, data: impl IntoIterator<Item = u8>)
 {
-	addr.relocations.push(Relocation { label, idx: out.len(), offset, instr_len });
+	out.bytes.extend(data);
+}
+
+fn push_byte(out: &mut Output, byte: u8)
+{
+	out.bytes.push(byte);
+}
+
+fn add_address(out: &mut Output, name: String, addr: usize)
+{
+	out.addresses.insert(name, addr);
+}
+
+fn get_address(out: &Output, name: &str) -> Option<usize>
+{
+	out.addresses.get(name).copied()
+}
+
+fn get_label_address(out: &Output, label: u16) -> Option<usize>
+{
+	get_address(out, &format!("L{label}"))
+}
+
+fn add_relocation(out: &mut Output, label: u16, offset: usize, instr_len: i32)
+{
+	let idx = out.bytes.len();
+
+	out.relocations.push(Relocation { label, idx, offset, instr_len });
 }
 
 pub fn construct_file(code: &[Instruction]) -> Vec<u8>
@@ -155,30 +183,29 @@ fn construct_binary(code: &[Instruction]) -> Vec<u8>
 
 fn construct_code(code: &[Instruction]) -> Code
 {
-	let mut out = Vec::new();
-	let mut addresses = Addresses { map: HashMap::new(), relocations: Vec::new() };
+	let mut out = empty_output();
 	let entrypoint;
 
 	for instr in code {
-		write_code_instr(instr, &mut addresses, &mut out);
+		write_code_instr(instr, &mut out);
 	}
 
-	entrypoint = out.len();
+	entrypoint = out.bytes.len();
 
-	write_code_epilogue(&addresses, &mut out);
+	write_code_epilogue(&mut out);
 
-	write_relocations(&mut out, &addresses);
+	write_relocations(&mut out);
 
-	Code { text: out, entrypoint }
+	Code { text: out.bytes, entrypoint }
 }
 
-fn write_code_instr(instr: &Instruction, addresses: &mut Addresses, out: &mut Vec<u8>)
+fn write_code_instr(instr: &Instruction, out: &mut Output)
 {
 	let address;
 
 	match instr {
 		Instruction::FuncPrologue { name, stack_used } => {
-			addresses.map.insert(name.clone(), out.len());
+			add_address(out, name.clone(), out.bytes.len());
 			write_fn_prologue(*stack_used, out);
 		}
 		Instruction::Move { to, from } => write_move(*to, *from, out),
@@ -186,34 +213,34 @@ fn write_code_instr(instr: &Instruction, addresses: &mut Addresses, out: &mut Ve
 		Instruction::Arith { op, dst, src } => write_arith(*op, *dst, *src, out),
 		Instruction::Return => write_return(out),
 		Instruction::FuncCall { name } => {
-			address = addresses.map.get(name).try_to(format!("find function {name:?}"));
-			write_fn_call(*address, out);
+			address = get_address(out, name).try_to(format!("find function {name:?}"));
+			write_fn_call(address, out);
 		}
 		Instruction::JumpCond { cond, label } => {
-			write_jump_cond(*cond, *label, addresses, out);
+			write_jump_cond(*cond, *label, out);
 		}
-		Instruction::Jump { label } => write_jump(*label, addresses, out),
+		Instruction::Jump { label } => write_jump(*label, out),
 		Instruction::Label { name } => {
-			addresses.map.insert(format!("L{name}"), out.len());
+			add_address(out, format!("L{name}"), out.bytes.len());
 		}
 		Instruction::Compare { x, y } => write_compare(*x, *y, out),
 		Instruction::CompareImm { x, value } => write_compare_imm(*x, *value, out),
 	}
 }
 
-fn write_fn_prologue(stack_used: i32, out: &mut Vec<u8>)
+fn write_fn_prologue(stack_used: i32, out: &mut Output)
 {
-	out.extend([0x55]); // push rbp
-	out.extend([0x48, 0x89, 0xe5]); // mov rbp, rsp
+	push(out, [0x55]); // push rbp
+	push(out, [0x48, 0x89, 0xe5]); // mov rbp, rsp
 
 	// sub rsp, {stack_used}
 	if let Ok(byte) = i8::try_from(stack_used) {
-		out.extend([0x48, 0x83, 0xec]);
-		out.push(byte as u8);
+		push(out, [0x48, 0x83, 0xec]);
+		push_byte(out, byte as u8);
 	}
 	else {
-		out.extend([0x48, 0x81, 0xec]);
-		out.extend(zero_extend(stack_used, 5));
+		push(out, [0x48, 0x81, 0xec]);
+		push(out, zero_extend(stack_used, 5));
 	}
 }
 
@@ -228,52 +255,52 @@ fn zero_extend(imm: i32, num_bytes: usize) -> Vec<u8>
 	out
 }
 
-fn write_move(to: Assignment, from: Assignment, out: &mut Vec<u8>)
+fn write_move(to: Assignment, from: Assignment, out: &mut Output)
 {
 	match (to, from) {
 		(Assignment::Stack(_), Assignment::Stack(_)) =>
 			error("moves from memory to memory are invalid"),
 		(Assignment::Stack(offset), src) => {
-			out.push(0x89);
+			push_byte(out, 0x89);
 			modrm(offset, src, out);
 		}
 		(dst, Assignment::Stack(offset)) => {
-			out.push(0x8b);
+			push_byte(out, 0x8b);
 			modrm(offset, dst, out);
 		}
 		(dst, src) => {
-			out.push(0x89);
+			push_byte(out, 0x89);
 			modrm_regs(dst, src, out);
 		}
 	}
 }
 
-fn modrm(offset: i32, src: Assignment, out: &mut Vec<u8>)
+fn modrm(offset: i32, src: Assignment, out: &mut Output)
 {
 	if let Ok(byte) = i8::try_from(offset) {
 		push_modrm(0b01, reg_field(src), 0b101, out); // [rbp+disp8]
-		out.push(byte as u8);
+		push_byte(out, byte as u8);
 	}
 	else {
 		push_modrm(0b10, reg_field(src), 0b101, out); // [rbp+disp32]
-		out.extend(offset.to_le_bytes());
+		push(out, offset.to_le_bytes());
 	}
 }
 
-fn modrm_regs(x: Assignment, y: Assignment, out: &mut Vec<u8>)
+fn modrm_regs(x: Assignment, y: Assignment, out: &mut Output)
 {
 	push_modrm(0b11, reg_field(y), reg_field(x), out);
 }
 
-fn modrm_single(offset: i32, digit: u8, out: &mut Vec<u8>)
+fn modrm_single(offset: i32, digit: u8, out: &mut Output)
 {
 	if let Ok(byte) = i8::try_from(offset) {
 		push_modrm(0b01, digit, 0b101, out); // [rbp+disp8]
-		out.push(byte as u8);
+		push_byte(out, byte as u8);
 	}
 	else {
 		push_modrm(0b10, digit, 0b101, out); // [rbp+disp32]
-		out.extend(offset.to_le_bytes());
+		push(out, offset.to_le_bytes());
 	}
 }
 
@@ -290,24 +317,24 @@ fn reg_field(register: Assignment) -> u8
 	}
 }
 
-fn push_modrm(modb: u8, reg: u8, rm: u8, out: &mut Vec<u8>)
+fn push_modrm(modb: u8, reg: u8, rm: u8, out: &mut Output)
 {
-	out.push((modb << 6) | (reg << 3) | rm);
+	push_byte(out, (modb << 6) | (reg << 3) | rm);
 }
 
-fn write_move_imm(dst: Assignment, value: i32, out: &mut Vec<u8>)
+fn write_move_imm(dst: Assignment, value: i32, out: &mut Output)
 {
 	match dst {
 		Assignment::Stack(offset) => {
-			out.push(0xc7);
+			push_byte(out, 0xc7);
 			modrm_single(offset, 0, out);
-			out.extend(value.to_le_bytes());
+			push(out, value.to_le_bytes());
 		}
 		_ => error(format!("unexpected move imm case {dst:?}")),
 	}
 }
 
-fn write_arith(op: ArithOp, dst: Assignment, src: Assignment, out: &mut Vec<u8>)
+fn write_arith(op: ArithOp, dst: Assignment, src: Assignment, out: &mut Output)
 {
 	let fmt;
 
@@ -323,132 +350,132 @@ fn write_arith(op: ArithOp, dst: Assignment, src: Assignment, out: &mut Vec<u8>)
 	}
 }
 
-fn write_arith_stack_reg(op: ArithOp, offset: i32, src: Assignment, out: &mut Vec<u8>)
+fn write_arith_stack_reg(op: ArithOp, offset: i32, src: Assignment, out: &mut Output)
 {
 	match op {
-		ArithOp::Add => out.push(0x01),
-		ArithOp::Sub => out.push(0x29),
+		ArithOp::Add => push_byte(out, 0x01),
+		ArithOp::Sub => push_byte(out, 0x29),
 	}
 
 	modrm(offset, src, out);
 }
 
-fn write_arith_reg_stack(op: ArithOp, src: Assignment, offset: i32, out: &mut Vec<u8>)
+fn write_arith_reg_stack(op: ArithOp, src: Assignment, offset: i32, out: &mut Output)
 {
 	match op {
-		ArithOp::Add => out.push(0x03),
-		ArithOp::Sub => out.push(0x2b),
+		ArithOp::Add => push_byte(out, 0x03),
+		ArithOp::Sub => push_byte(out, 0x2b),
 	}
 
 	modrm(offset, src, out);
 }
 
-fn write_return(out: &mut Vec<u8>)
+fn write_return(out: &mut Output)
 {
-	out.push(0xc9); // leave
-	out.push(0xc3); // ret
+	push_byte(out, 0xc9); // leave
+	push_byte(out, 0xc3); // ret
 }
 
-fn write_fn_call(address: usize, out: &mut Vec<u8>)
+fn write_fn_call(address: usize, out: &mut Output)
 {
-	let offset = rip_offset(address, out, 5);
+	let offset = rip_offset(address, &out.bytes, 5);
 
-	out.push(0xe8); // call
-	out.extend(offset.to_le_bytes());
+	push_byte(out, 0xe8); // call
+	push(out, offset.to_le_bytes());
 }
 
-fn rip_offset(address: usize, out: &[u8], instr_len: i32) -> i32
+fn rip_offset(address: usize, bytes: &[u8], instr_len: i32) -> i32
 {
-	let idx = i32::try_from(out.len()).or_err("code len overflows u32");
+	let idx = i32::try_from(bytes.len()).or_err("code len overflows u32");
 	let address = i32::try_from(address).or_err("address overflows u32");
 
 	address - idx - instr_len
 }
 
-fn write_jump_cond(cond: Cond, label: u16, addresses: &mut Addresses, out: &mut Vec<u8>)
+fn write_jump_cond(cond: Cond, label: u16, out: &mut Output)
 {
 	let offset;
 
-	offset = if let Some(address) = addresses_get_label(addresses, label) {
-		rip_offset(address, out, 6)
+	offset = if let Some(address) = get_label_address(out, label) {
+		rip_offset(address, &out.bytes, 6)
 	}
 	else {
-		addresses_push_reloc(addresses, label, out, 2, 6);
+		add_relocation(out, label, 2, 6);
 		0
 	};
 
-	out.push(0x0f);
+	push_byte(out, 0x0f);
 
 	match cond {
-		Cond::LT => out.push(0x8c),
-		Cond::GT => out.push(0x8f),
-		Cond::LTE => out.push(0x8e),
-		Cond::GTE => out.push(0x8d),
-		Cond::NotEqual => out.push(0x85),
-		Cond::Equal => out.push(0x84),
+		Cond::LT => push_byte(out, 0x8c),
+		Cond::GT => push_byte(out, 0x8f),
+		Cond::LTE => push_byte(out, 0x8e),
+		Cond::GTE => push_byte(out, 0x8d),
+		Cond::NotEqual => push_byte(out, 0x85),
+		Cond::Equal => push_byte(out, 0x84),
 	}
 
-	out.extend(offset.to_le_bytes());
+	push(out, offset.to_le_bytes());
 }
 
-fn write_jump(label: u16, addresses: &mut Addresses, out: &mut Vec<u8>)
+fn write_jump(label: u16, out: &mut Output)
 {
 	let offset;
 
-	offset = if let Some(address) = addresses_get_label(addresses, label) {
-		rip_offset(address, out, 5)
+	offset = if let Some(address) = get_label_address(out, label) {
+		rip_offset(address, &out.bytes, 5)
 	}
 	else {
-		addresses_push_reloc(addresses, label, out, 1, 5);
+		add_relocation(out, label, 1, 5);
 		0
 	};
 
-	out.push(0xe9); // jmp
+	push_byte(out, 0xe9); // jmp
 
-	out.extend(offset.to_le_bytes());
+	push(out, offset.to_le_bytes());
 }
 
-fn write_compare(x: Assignment, y: Assignment, out: &mut Vec<u8>)
+fn write_compare(x: Assignment, y: Assignment, out: &mut Output)
 {
 	match (x, y) {
 		(Assignment::Stack(_), Assignment::Stack(_)) =>
 			error("comparisons between two memory locations are invalid"),
 		(src, Assignment::Stack(offset)) => {
-			out.push(0x3b);
+			push_byte(out, 0x3b);
 			modrm(offset, src, out);
 		}
 		_ => error(format!("unexpected comparison case: {x:?} {y:?}")),
 	}
 }
 
-fn write_compare_imm(x: Assignment, value: i32, out: &mut Vec<u8>)
+fn write_compare_imm(x: Assignment, value: i32, out: &mut Output)
 {
 	if let Assignment::Stack(offset) = x {
-		out.push(0x81);
+		push_byte(out, 0x81);
 		modrm_single(offset, 7, out);
-		out.extend(value.to_le_bytes());
+		push(out, value.to_le_bytes());
 		return;
 	}
 
 	error(format!("unexpected case of comparison with immediate: {x:?}"));
 }
 
-fn write_code_epilogue(addresses: &Addresses, out: &mut Vec<u8>)
+fn write_code_epilogue(out: &mut Output)
 {
-	let main = addresses.map.get("main").try_to("find main function");
+	let main = get_address(out, "main").try_to("find main function");
 
-	write_fn_call(*main, out);
+	write_fn_call(main, out);
 
-	out.extend([0x48, 0x89, 0xc7]); // mov rdi, rax
+	push(out, [0x48, 0x89, 0xc7]); // mov rdi, rax
 
 	// mov rax, 60
-	out.push(0xb8);
-	out.extend(zero_extend(60, 4));
+	push_byte(out, 0xb8);
+	push(out, zero_extend(60, 4));
 
-	out.extend([0x0f, 0x05]); // syscall
+	push(out, [0x0f, 0x05]); // syscall
 }
 
-fn write_relocations(out: &mut [u8], addr: &Addresses)
+fn write_relocations(out: &mut Output)
 {
 	let mut label_str;
 	let mut address;
@@ -457,17 +484,17 @@ fn write_relocations(out: &mut [u8], addr: &Addresses)
 	let mut offset;
 	let mut location;
 
-	for relocation in &addr.relocations {
+	for relocation in &out.relocations {
 		label_str = format!("L{}", relocation.label);
-		address = addr.map.get(&label_str).try_to(format!("find label {label_str}"));
+		address = get_address(out, &label_str).try_to(format!("find label {label_str}"));
 
 		location = relocation.idx + relocation.offset;
 
 		idx = i32::try_from(relocation.idx).or_err("code location overflows u32");
-		addr_i32 = i32::try_from(*address).or_err("address overflows u32");
+		addr_i32 = i32::try_from(address).or_err("address overflows u32");
 
 		offset = addr_i32 - idx - relocation.instr_len;
 
-		out[location..location + 4].copy_from_slice(&offset.to_le_bytes());
+		out.bytes[location..location + 4].copy_from_slice(&offset.to_le_bytes());
 	}
 }
